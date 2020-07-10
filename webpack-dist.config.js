748571800d1c4ae37528631b0e221c30fcc6e67d
mac <mac@macdeMacBook-Pro.local>
2020-07-10 13:56:07 +0800

    hebing

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -59,7 +59,7 @@ module.exports = {
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        '' // 这里注意改成你的在线简历的网站
+        'https://github.com/qytayh/resume' // 这里注意改成你的在线简历的网站
       ]);
     }),
   ]

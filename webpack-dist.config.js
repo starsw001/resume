3bfc7fc2e3a4a919cbfe7f1554176cfa738a0542
mac <mac@macdeMacBook-Pro.local>
2020-07-10 13:02:58 +0800

    hebing

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -12,7 +12,7 @@ const outputPath = path.resolve(__dirname, 'docs');
 module.exports = {
   output: {
     path: outputPath,
-    publicPath: '',
+    publicPath: '', 
     filename: '[name]_[chunkhash:8].js',
   },
   resolve: {
@@ -54,12 +54,12 @@ module.exports = {
     new OptimizeCSSAssetsPlugin(),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.wuhaolin.cn');
+      // fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.wuhaolin.cn');
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://resume.wuhaolin.cn' // 这里注意改成你的在线简历的网站
+        '' // 这里注意改成你的在线简历的网站
       ]);
     }),
   ]
